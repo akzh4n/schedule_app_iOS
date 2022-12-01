c3f98254a7849f944cdbefa601af3006746a4b32
manste1n <akzhan.kaz2003@gmail.com>
2022-12-01 15:03:06 +0500

    Initial Commit

---
+++ schedule_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  schedule_app
+//
+//  Created by Акжан Калиматов on 01.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
